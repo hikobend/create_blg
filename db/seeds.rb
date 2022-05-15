6927cd0cead0d1fe1c736f86a17308a058fddd94
coffee <yamanakakohei@MBP.local>
2022-05-15 15:35:32 +0900

    tag追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,5 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 User.create!(name: 'Aaユーザー1', email: 'a@a.com', password: 'aaaaaa')
+tag.create!(name: 'AWS')
+tag.create!(name: 'Ruby on Rails')
