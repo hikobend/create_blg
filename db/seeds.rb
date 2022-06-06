cc397e19ff6a41c6e0054ca6abcec6032e518b47
hikobend <hiko.bkend@gmail.com>
2022-06-07 07:17:31 +0900

    redcarpet搭載

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,3 @@
-# frozen_string_literal: true
-
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
 User.create!(name: 'Aaユーザー1', email: 'a@a.com', password: 'aaaaaa')
 Tag.create!(name: 'Docker')
 Tag.create!(name: 'Ruby on Rails')
