4f2ebc68c6c8ab6d933bab8241326a1f6d5a9a20
hikobend <hiko.bkend@gmail.com>
2022-06-13 23:33:33 +0900

    編集画面・削除機能追加

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -18,10 +18,31 @@ class PostsController < ApplicationController
     if @post.save
       redirect_to posts_path, notice: '追加に成功しました'
     else
-      # flash.now[:notice]が反応しない
       flash.now[:notice] = '追加に失敗しました'
       render :new
     end
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
+      redirect_to posts_path, notice: '変更に成功しました'
+    else
+      # flash.now[:notice]が反応しない
+      flash.now[:notice] = '変更に失敗しました'
+      render :edit
+    end
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    flash[:alert] = '削除しました。'
+    redirect_to posts_path
   end
 
   private
