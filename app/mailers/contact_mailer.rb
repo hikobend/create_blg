8bcde41dc6ec669c3aa6e2e1532b740a0648e03a
hikobend <hiko.bkend@gmail.com>
2022-06-20 22:03:39 +0900

    お問合せフォーム削除

--- app/mailers/contact_mailer.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-class ContactMailer < ApplicationMailer
-  def send_mail(contact)
-    @contact = contact
-    mail to:   ENV['TOMAIL'], subject: "【お問い合わせ】#{@contact.subject_i18n}"
-  end
-end
