148ebc457fa93462384e5d1da99c91b189db3e37
hikobend <hiko.bkend@gmail.com>
2022-06-23 22:02:05 +0900

    一旦完成

--- spec/factories/inquiries.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-FactoryBot.define do
-  factory :inquiry do
-    name { 'MyString' }
-    message { 'MyString' }
-  end
-end
--- spec/factories/members.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-FactoryBot.define do
-  factory :member do
-    name_sei { 'MyString' }
-    name_mei { 'MyString' }
-    email { 'MyString' }
-  end
-end
